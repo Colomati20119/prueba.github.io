3b9e8f00a3336196d00226f7857b12b159e3396e
Colomati20119 <78377886+Colomati20119@users.noreply.github.com>
2024-07-29 17:18:06 -0300

    initial commit

---
+++ junior - copia/main.js
@@ -0,0 +1,8 @@
+function mostrar(id){
+    var div = document.getElementById(id);
+    div.style.display = 'block'
+}
+function ocultar(id){
+    var div = document.getElementById(id);
+    div.style.display = 'none'
+}
