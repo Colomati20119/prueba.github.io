8b559821c7eb7313da33af4bb6eb7860bf7772ee
Colomati20119 <78377886+Colomati20119@users.noreply.github.com>
2024-07-29 17:20:52 -0300

    Delete junior - copia directory

--- junior - copia/main.js
+++
@@ -1,8 +0,0 @@
-function mostrar(id){
-    var div = document.getElementById(id);
-    div.style.display = 'block'
-}
-function ocultar(id){
-    var div = document.getElementById(id);
-    div.style.display = 'none'
-}
